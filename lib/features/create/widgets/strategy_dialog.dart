80013289a1b85370459d57ba2180bac6d0df5f0d
wangpf1129 <wangpf2051@gmail.com>
2025-03-28 15:52:24 +0800

    修复键盘顶出区域

--- lib/features/create/widgets/strategy_dialog.dart
+++ lib/features/create/widgets/strategy_dialog.dart
@@ -23,55 +23,57 @@ class StrategyDialog extends HookWidget {
       child: Stack(clipBehavior: Clip.none, children: [
         Padding(
           padding: const EdgeInsets.all(22),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              const Text(
-                '设置辩论策略（进阶）',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
-              ),
-              const SizedBox(
-                height: 22,
-              ),
-              Container(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    color: Colors.white.withOpacity(0.1),
-                    borderRadius: BorderRadius.circular(8),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const Text(
+                  '设置辩论策略（进阶）',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
                   ),
-                  child: TextField(
-                    controller: textController,
-                    keyboardType: TextInputType.multiline,
-                    maxLines: 10,
-                    // 底部有白色边框给移除掉
-                    decoration: const InputDecoration(
-                      border: InputBorder.none,
+                ),
+                const SizedBox(
+                  height: 22,
+                ),
+                Container(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: Colors.white.withOpacity(0.1),
+                      borderRadius: BorderRadius.circular(8),
                     ),
-                  )),
-              const SizedBox(
-                height: 22,
-              ),
-              PrimaryButton(
-                text: '确定',
-                onPressed: () {
-                  if (textController.text.isEmpty) {
-                    // toast
-                    Fluttertoast.showToast(
-                      msg: '请输入辩论策略',
-                      gravity: ToastGravity.CENTER,
-                    );
-                    return;
-                  }
-                  onStrategyChanged(textController.text);
-                  Navigator.of(context).pop();
-                },
-              ),
-            ],
+                    child: TextField(
+                      controller: textController,
+                      keyboardType: TextInputType.multiline,
+                      maxLines: 10,
+                      // 底部有白色边框给移除掉
+                      decoration: const InputDecoration(
+                        border: InputBorder.none,
+                      ),
+                    )),
+                const SizedBox(
+                  height: 22,
+                ),
+                PrimaryButton(
+                  text: '确定',
+                  onPressed: () {
+                    if (textController.text.isEmpty) {
+                      // toast
+                      Fluttertoast.showToast(
+                        msg: '请输入辩论策略',
+                        gravity: ToastGravity.CENTER,
+                      );
+                      return;
+                    }
+                    onStrategyChanged(textController.text);
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
+            ),
           ),
         ),
         // 右上角关闭按钮
